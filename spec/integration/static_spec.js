0ff1dc0fb0b31e3cc6e13decb1687fdbcb53eb36
ocsfwarch <webmaster@ocsfwarch.com>
2019-06-16 09:20:36 -0700

    Added routing-control-view components

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -9,7 +9,11 @@ describe("routes : static", () => {
 
 //#2
     it("should return status code 200", (done) => {
-
+    //it("should return status code 200 and have 'Welcome to Bloccit 2' in the body of the response", (done) => {
+    //    request.get(base, (err, res, body) => {
+    //      expect(res.statusCode).toBe(200);
+    //      expect(body).toContain("Welcome to Bloccit 2");
+     
 //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
